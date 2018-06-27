1731873a2368a1d662f17a5d2e499b19f13da2b1
Colorado Stark <coloradostark@yahoo.com>
2018-06-28 01:21:41 +0300

    Add files via upload

---
+++ provisioner.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+sudo apt-get install python-software-properties -y
+sudo LC_ALL=en_US.UTF-8 add-apt-repository ppa:ondrej/php -y
+sudo apt-get update
+sudo apt-get install php7.2 php7.2-fpm php7.2-mysql -y
+sudo apt-get --purge autoremove -y
+sudo service php7.2-fpm restart
+
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password root'
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password root'
+sudo apt-get -y install mysql-server mysql-client
+sudo service mysql start
+
+sudo apt-get install nginx -y
+sudo cat > /etc/nginx/sites-available/default <<- EOM
+server {
+    listen 80 default_server;
+    listen [::]:80 default_server ipv6only=on;
+
+    root /vagrant;
+    index index.php index.html index.htm;
+
+    server_name server_domain_or_IP;
+
+    location / {
+        try_files \$uri \$uri/ /index.php?\$query_string;
+    }
+
+    location ~ \.php\$ {
+        try_files \$uri /index.php =404;
+        fastcgi_split_path_info ^(.+\.php)(/.+)\$;
+        fastcgi_pass unix:/var/run/php/php7.0-fpm.sock;
+        fastcgi_index index.php;
+        fastcgi_param SCRIPT_FILENAME \$document_root\$fastcgi_script_name;
+        include fastcgi_params;
+    }
+}
+EOM
+sudo service nginx restart
