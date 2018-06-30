7dd81ac7b3545245f8e749f2756e15c08fe4362e
Colorado Stark <coloradostark@yahoo.com>
2018-06-30 23:02:51 +0300

    Add files via upload
    Added the commands to install composer

--- provisioner.sh
+++ provisioner.sh
@@ -50,3 +50,9 @@ server {
 }
 EOM
 sudo service nginx restart
+
+sudo php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+php composer-setup.php --install-dir=/usr/local/bin --filename=composer
+php -r "unlink('composer-setup.php');"
+
+
