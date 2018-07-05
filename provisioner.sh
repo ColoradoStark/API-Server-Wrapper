a4fe62a3fd61e22c3d1d1db16720a3015f68e476
Colorado Stark <coloradostark@yahoo.com>
2018-07-06 01:47:04 +0300

    Cleaned up
    Removed Commented out portions
    Uninstalled Expect

--- provisioner.sh
+++ provisioner.sh
@@ -9,19 +9,6 @@ sudo apt-get --purge autoremove -y
 sudo service php7.2-fpm restart
 
 
-#export DEBIAN_FRONTEND="noninteractive"
-
-
-#sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password root'
-#sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password root'
-
-#sudo apt-key adv --keyserver pgp.mit.edu --recv-keys 5072E1F5
-#sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 8C718D3B5072E1F5
-#cat <<- EOF > /etc/apt/sources.list.d/mysql.list
-#deb http://repo.mysql.com/apt/ubuntu/ trusty mysql-5.7
-#EOF
-#sudo apt-get update
-
 export DEBIAN_FRONTEND=noninteractive
 
 MYSQL_ROOT_PASSWORD='root' # SET THIS! Avoid quotes/apostrophes in the password, but do use lowercase + uppercase + numbers + special chars
@@ -67,18 +54,15 @@ send "y\r"
 
 EOF
 
-# Run Expect script.
-# This runs the "mysql_secure_installation" script which removes insecure defaults.
+
 sudo expect ~/secure_our_mysql.sh
 
 # Cleanup
-rm -v ~/secure_our_mysql.sh # Remove the generated Expect script
-#sudo apt-get -qq purge expect > /dev/null # Uninstall Expect, commented out in case you need Expect
+rm -v ~/secure_our_mysql.sh 
+sudo apt-get -qq purge expect > /dev/null 
 
 echo "MySQL setup completed. Insecure defaults are gone. Please remove this script manually when you are done with it (or at least remove the MySQL root password that you put inside it."
 
-
-#sudo apt-get -y install mysql-server mysql-client
 sudo service mysql start
 
 sudo apt-get install nginx -y
@@ -108,7 +92,7 @@ server {
 EOM
 sudo service nginx restart
 
-cd /usr/share/nginx/html #/API
+cd /usr/share/nginx/html 
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 php composer-setup.php --prefer-dist --no-plugins --no-scripts
 php -r "unlink('composer-setup.php');"
