1c2ff980f3b3e7823ab43102cbb6699d6c775b69
Colorado Stark <coloradostark@yahoo.com>
2018-07-09 01:29:12 +0300

    Demo Slim App
    Switched the sanity check index test page to the Hello/{name} demo of the slim app API

--- Test-Pages/index.php
+++ Test-Pages/index.php
@@ -1,3 +1,41 @@
 <?php
-echo 'Sanity Check:  ';
-echo 'Yes, It is working.';
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+if (PHP_SAPI == 'cli-server') {
+    // To help the built-in PHP dev server, check if the request was actually for
+    // something which should probably be served as a static file
+    $url  = parse_url($_SERVER['REQUEST_URI']);
+    $file = __DIR__ . $url['path'];
+    if (is_file($file)) {
+        return false;
+    }
+}
+
+require __DIR__ . '/../vendor/autoload.php';
+
+session_start();
+
+// Instantiate the app
+//$settings = require __DIR__ . '/../src/settings.php';
+//$app = new \Slim\App($settings);
+
+$app = new \Slim\App;
+$app->get('/hello/{name}', function (Request $request, Response $response, array $args) {
+    $name = $args['name'];
+    $response->getBody()->write("Hello, $name");
+
+    return $response;
+});
+
+// Set up dependencies
+require __DIR__ . '/../src/dependencies.php';
+
+// Register middleware
+require __DIR__ . '/../src/middleware.php';
+
+// Register routes
+require __DIR__ . '/../src/routes.php';
+
+// Run app
+$app->run();
